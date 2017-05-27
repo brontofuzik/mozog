41606c1cce31f7d31e89dbd1dd04069e09403770
Brontofuzik <brontofuzik86@gmail.com>
2017-05-27 19:33:40 +0200

    ACO: Function optimization example - objective functions

---
+++ ACO/AntColonyOptimization.Examples/FunctionOptimization.cs
@@ -0,0 +1,45 @@
+﻿namespace AntColonyOptimization.Examples
+{
+    static class FunctionOptimization
+    {
+        public static AntColonyOptimization SphereModelFunction
+            => new AntColonyOptimization(6) { Objective = new SphereModel(6) };
+
+        public static AntColonyOptimization GoldsteinPriceFunction
+            => new AntColonyOptimization(2) { Objective = new GoldsteinPrice(2) };
+
+        public static AntColonyOptimization RosenbrockFunction
+            => new AntColonyOptimization(2) { Objective = new Rosenbrock(2) };
+
+        public static AntColonyOptimization ZakharovFunction
+            => new AntColonyOptimization(2) { Objective = new Zakharov(2) };
+
+        private class SphereModel : ObjectiveFunction
+        {
+            public SphereModel(int dimension) : base(dimension, Objective.Minimize) {}
+
+            protected override double EvaluateInternal(double[] steps) => Mozog.Examples.Functions.SphereModel(steps);
+        }
+
+        private class GoldsteinPrice : ObjectiveFunction
+        {
+            public GoldsteinPrice(int dimension) : base(dimension, Objective.Minimize) {}
+
+            protected override double EvaluateInternal(double[] steps) => Mozog.Examples.Functions.GoldsteinPrice(steps);
+        }
+
+        private class Rosenbrock : ObjectiveFunction
+        {
+            public Rosenbrock(int dimension) : base(dimension, Objective.Minimize) {}
+
+            protected override double EvaluateInternal(double[] steps) => Mozog.Examples.Functions.Rosenbrock(steps);
+        }
+
+        private class Zakharov : ObjectiveFunction
+        {
+            public Zakharov(int dimension) : base(dimension, Objective.Minimize) {}
+
+            protected override double EvaluateInternal(double[] steps) => Mozog.Examples.Functions.Zakharov(steps);
+        }
+    }
+}
--- ACO/AntColonyOptimization.Examples/Program.cs
+++ ACO/AntColonyOptimization.Examples/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using AntColonyOptimization.Examples.FunctionOptimization;
 using Mozog.Utils;
 
 namespace AntColonyOptimization.Examples
@@ -7,28 +6,21 @@ namespace AntColonyOptimization.Examples
     internal class Program
     {
         public static void Main(string[] args)
-        {
-            
-            Run("Sphere model function (SM)",
-                new AntColonyOptimization(6) {Objective = new SphereModelFunction(6)}, 
+        {        
+            Run("Sphere model function (SM)", FunctionOptimization.SphereModelFunction, 
                 ants: 8, pdfs: 3,
                 targetEvaluation: Double.MinValue, maxIterations: 10_000);
-
             
-            Run("Goldstein and Price function (GP)",
-                new AntColonyOptimization(2) {Objective = new GoldsteinPriceFunction(2)},
+            Run("Goldstein and Price function (GP)", FunctionOptimization.GoldsteinPriceFunction,
                 ants: 6, pdfs: 4,
                 targetEvaluation: Double.MinValue, maxIterations: 10_000);
-
             
-            Run("Rosenbrock function (R2)",
-                new AntColonyOptimization(2) {Objective = new RosenbrockFunction(2)},
+            Run("Rosenbrock function (R2)", FunctionOptimization.RosenbrockFunction,
                 ants: 30, pdfs: 8,
                 targetEvaluation: Double.MinValue, maxIterations: 10_000);
 
             
-            Run("Zakharov function (Z2)",
-                new AntColonyOptimization(2) {Objective = new ZakharovFunction(2)},
+            Run("Zakharov function (Z2)", FunctionOptimization.ZakharovFunction,
                 ants: 8, pdfs: 4,
                 targetEvaluation: Double.MinValue, maxIterations: 10_000);
         }
