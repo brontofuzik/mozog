6a86b4b49dfa3a8532cc80719f99621a5bad693a
Brontofuzik <brontofuzik86@gmail.com>
2017-05-28 01:13:41 +0200

    NN: SA trainer

--- NN/NeuralNetwork.Training.Annealing/SimulatedAnnealingTrainer.cs
+++ NN/NeuralNetwork.Training.Annealing/SimulatedAnnealingTrainer.cs
@@ -1,43 +1,53 @@
-﻿using NeuralNetwork.Interfaces;
+﻿using System;
+using System.Linq;
+using Mozog.Utils;
+using Mozog.Utils.Math;
+using NeuralNetwork.Data;
+using NeuralNetwork.Interfaces;
+using SimulatedAnnealing;
+using SimulatedAnnealing.Functions.Cooling;
+using SimulatedAnnealing.Functions.Initialization;
+using SimulatedAnnealing.Functions.Objective;
+using SimulatedAnnealing.Functions.Perturbation;
 
 namespace NeuralNetwork.Training.Annealing
 {
-    public class SimulatedAnnealingTrainer : TrainerBase
+    public class SimulatedAnnealingTrainer : TrainerBase<TrainingArgs>
     {
-        private readonly NetworkSimlatedAnnealing networkSimulatedAnnealing = new NetworkSimlatedAnnealing();
-
-        public SimulatedAnnealingTrainer(TrainingSet trainingSet, TrainingSet validationSet, TrainingSet testSet)
-            : base(trainingSet, validationSet, testSet)
+        public override TrainingLog Train(INetwork network, IDataSet data, TrainingArgs args)
         {
+            var networkSimulatedAnnealing = new SimulatedAnnealing<double>(network.SynapseCount)
+            {
+                Objective = ObjectiveFunction<double>.Minimize(weights =>
+                {
+                    network.SetWeights(weights);
+                    return network.CalculateError((DataSet)data);
+                }),
+                Initialization = new LambdaInitialization<double>(d => d.Times(() => StaticRandom.Double(-20, +20)).ToArray()),
+                Perturbation = new LambdaPerturbation<double>(weights =>
+                {
+                    double[] newWeights = new double[weights.Length];
+                    Array.Copy(weights, newWeights, weights.Length);
+
+                    int index = StaticRandom.Int(0, newWeights.Length);
+                    newWeights[index] = (newWeights[index] + StaticRandom.Double(-20, +20)) / 2.0;
+
+                    return newWeights;
+                }),
+                Cooling = LambdaCooling.Exponential
+            };
+
+            var result = networkSimulatedAnnealing.Run(initialTemperature: 1000.0, finalTemperature: 0.001,
+                targetEnergy: args.MaxError, maxIterations: args.MaxIterations);
+            network.SetWeights(result.State.S);
+
+            return new TrainingLog(0);
         }
 
-        public override string Name => "SimulatedAnnealingTrainer";
+        // TODO
+        public override event EventHandler<TrainingStatus> WeightsUpdated;
 
-        public override TrainingLog Train(INetwork network, int maxIterationCount, double maxTolerableNetworkError)
-        {
-            // The network simulated annealing parameters.
-            NetworkObjectiveFunction networkObjectiveFunction = new NetworkObjectiveFunction(network, trainingSet);
-            double initialTemperature = 1000.0;
-            double finalTemperature = 0.001;
-
-            // Train the network.
-            int iterationCount;
-            double networkError;
-            double[] weights = networkSimulatedAnnealing.Run(networkObjectiveFunction,
-                maxIterationCount, out iterationCount, maxTolerableNetworkError, out networkError,
-                initialTemperature, finalTemperature);
-            network.SetWeights(weights);
-
-            // LOGGING
-            // -------
-
-            // Create the training log and log the training data.
-            TrainingLog trainingLog = new TrainingLog(iterationCount, networkError);
-
-            // Log the network statistics.
-            LogNetworkStatistics(trainingLog, network);
-
-            return trainingLog;
-        }
+        // TODO
+        public override event EventHandler WeightsReset;
     }
 }
