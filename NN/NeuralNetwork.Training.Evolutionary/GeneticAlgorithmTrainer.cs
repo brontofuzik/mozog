c7b0b63e796cff14fd5191af68a35b431afcc650
Brontofuzik <brontofuzik86@gmail.com>
2017-05-28 00:43:06 +0200

    NN: GA trainer

--- NN/NeuralNetwork.Training.Evolutionary/GeneticAlgorithmTrainer.cs
+++ NN/NeuralNetwork.Training.Evolutionary/GeneticAlgorithmTrainer.cs
@@ -1,31 +1,36 @@
-﻿using GeneticAlgorithm;
+﻿using System;
+using GeneticAlgorithm;
+using Mozog.Utils.Math;
+using NeuralNetwork.Data;
 using NeuralNetwork.Interfaces;
 
 namespace NeuralNetwork.Training.Evolutionary
 {
-    public class GeneticAlgorithmTrainer : TrainerBase
+    public class GeneticAlgorithmTrainer : TrainerBase<TrainingArgs>
     {
-        // TODO
-        //private readonly GeneticAlgorithm<double> geneticAlgo;
-
-        public GeneticAlgorithmTrainer(TrainingSet trainingSet, TrainingSet validationSet, TrainingSet testSet)
-            : base(trainingSet, validationSet, testSet)
+        public override TrainingLog Train(INetwork network, IDataSet data, TrainingArgs args)
         {
-        }
+            var geneticAlgo = new GeneticAlgorithm<double>(network.SynapseCount).Configure(cfg => cfg
+                .Fitness.Minimize(chromosome =>
+                {
+                    network.SetWeights(chromosome);
+                    return network.CalculateError((DataSet)data);
+                })
+                .Initialization.Piecewise(() => StaticRandom.Double(-10, +10))
+                .Crossover.SinglePoint()
+                .Mutation.RandomPoint(gene => (gene + StaticRandom.Double(-10, +10)) / 2.0)
+                .Termination.MaxGenerationsOrAcceptableEvaluation(maxGenerations: args.MaxIterations, acceptableEvaluation: args.MaxError));
 
-        public override string Name => "GeneticAlgorithmTrainer";
-
-        public override TrainingLog Train(INetwork network, int maxIterationCount, double maxTolerableNetworkError)
-        {
-            var geneticAlgo = NetworkGeneticAlgorithm.Algorithm(network, trainingSet, maxTolerableNetworkError, maxIterationCount);
-
-            Result<double> result = geneticAlgo.Run(populationSize: 500, crossoverRate: 0.8, mutationRate: 0.05);  
+            var result = geneticAlgo.Run(populationSize: 500, crossoverRate: 0.8, mutationRate: 0.05);
             network.SetWeights(result.Solution);
 
-            TrainingLog trainingLog = new TrainingLog(result.Generations, result.Evaluation);
-            LogNetworkStatistics(trainingLog, network);
-
-            return trainingLog;
+            return new TrainingLog(result.Generations);
         }
+
+        // TODO
+        public override event EventHandler<TrainingStatus> WeightsUpdated;
+
+        // TODO
+        public override event EventHandler WeightsReset;
     }
 }
